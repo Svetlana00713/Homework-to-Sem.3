132920ea77c4af56492551ecac362682ed9018ec
Svetlana <start61@mail.ru>
2023-02-20 20:35:42 +0300

    Added Task 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -7,7 +7,7 @@
 System.Console.Write("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 1; i <= N; i++)// i = i + 2
+for (int i = 1; i <= N; i++)
 {
     System.Console.Write($"{Math.Pow(i, 3)} ");
 }
