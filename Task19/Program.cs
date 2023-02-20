238d6048543272a942e800e7d02e32a681d8be4c
Svetlana <start61@mail.ru>
2023-02-20 20:24:28 +0300

    Added tasks 19, 21, 23

---
+++ Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string aTxt = Convert.ToString(a);
+char[] ar = aTxt.ToCharArray();
+Array.Reverse(ar);
+aTxt = new String(ar);
+int b = Convert.ToInt32(aTxt);
+if(a - b == 0)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+};
